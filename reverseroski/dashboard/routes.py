d3dd8ec1c53096ad4790f2531042b5c8a7114600
JC <johncunniff1248@gmail.com>
2018-12-11 23:24:07 -0500

    add dashboard

---
+++ reverseroski/dashboard/routes.py
@@ -0,0 +1,32 @@
+from flask import request, redirect, url_for, flash, render_template, Blueprint
+from flask_login import current_user, login_user, logout_user, login_required
+from sqlalchemy.exc import IntegrityError
+
+from ..app import db
+from ..models import Client, Command
+
+dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+
+
+@client.route('/')
+def serve_client():
+    return ''
+
+# @client.route('/register', methods=['POST'])
+# def register_client():
+#     form = RegisterClientForm()
+#     if form.validate_on_submit():
+#         try:
+#             c=Client(
+#                 clientname=form.clientname.data,
+#                 uname=form.uname.data,
+#                 registration_time=datetime.utcnow(),
+#             )
+#             db.session.add(c)
+#             db.session.commit()
+#             return json.dumps({
+#                 'hostid':str(c.get_id()),
+#             })
+#         except IntegrityError:
+#             db.session.rollback()
+#     return ''
