53d2495cafd9c31a2ff5a154a35d516575a7bbcc
JC <johncunniff1248@gmail.com>
2018-12-11 21:04:57 -0500

    update models and config

--- reverseroski/config.py
+++ reverseroski/config.py
@@ -1,7 +1,6 @@
 import os
+
 class Config():
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    THUMBNAIL_SIZE = 256
-    UPLOAD_PATH = 'uploads'
