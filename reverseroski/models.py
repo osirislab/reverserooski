a9654245d3e7b28485d40f3f6fc86da1771708df
JC <johncunniff1248@gmail.com>
2018-12-14 15:58:18 -0500

    new models for dashboard

--- reverseroski/models.py
+++ reverseroski/models.py
@@ -2,6 +2,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from .app import db
 
+class struct:
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            self.__setattr__(key, val)
+
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(128), unique=True, index=True)
@@ -16,6 +21,24 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+class Command(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    clientid=db.Column(db.Integer, unique=False, index=False)
+    command=db.Column(db.String(256), unique=False, index=False)
+    pending=db.Column(db.Boolean(), unique=False, index=False)
+    stdout=db.Column(db.Text(), unique=False, index=False)
+
+    def get_clientid(self):
+        return self.clientid
+
+    def get_command(self):
+        return self.command
+
+    def get_id(self):
+        return self.id
+
+    def set_stdout(self, stdout):
+        self.stdout=stdout
 
 class Client(db.Model):
     id=db.Column(db.Integer, primary_key=True)
@@ -29,18 +52,17 @@ class Client(db.Model):
     def get_id(self):
         return self.id
 
+    def get_all_commands(self):
+        return Command.query.filter_by(
+            clientid=self.id
+        ).all()
 
-class Command(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    clientid=db.Column(db.Integer, unique=False, index=False)
-    command=db.Column(db.String(256), unique=False, index=False)
-    pending=db.Column(db.Boolean(), unique=False, index=False)
+    def get_pending_commands(self):
+        return Command.query.filter_by(
+            clientid=self.id,
+            pending=True,
+        ).all()
 
-    def get_clientid(self):
-        return self.clientid
-
-    def get_command(self):
-        return self.command
-
-    def get_id(self):
-        return self.id
+class NavItem(struct):
+    text=''
+    link=''
